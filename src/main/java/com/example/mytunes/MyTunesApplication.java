5e434456320b266e9c46d3a5cc17cd0cbb08a62e
Jakoc <damernesjacob@gmail.com>
2023-11-29 09:23:34 +0100

    FRONT END 1

--- src/main/java/com/example/mytunes/MyTunesApplication.java
+++ src/main/java/com/example/mytunes/MyTunesApplication.java
@@ -11,7 +11,7 @@ public class MyTunesApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyTunesApplication.class.getResource("MyTunes.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 868, 483);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
